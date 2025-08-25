8c2c17b8a0f278d0b0c545f3961699f6d7dfd337
Júlia Alves Pereira <juliaalvespereira01@gmail.com>
2025-08-25 17:31:08 -0300

    Delete 20-08 - BackEnd/ex013.py

--- 20-08 - BackEnd/ex013.py
+++
@@ -1,9 +0,0 @@
-def eh_par(num):
-    if num % 2 == 0:
-        par = input(True)
-    else:
-        impar = input(False)
-    
-num = int(input("Digite o número e verifique se ele é par: "))
-
-eh_par(num)
